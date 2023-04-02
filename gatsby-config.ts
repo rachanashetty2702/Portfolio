0fcc8b0695c756cf1119875eab5028243bf76fd5
Rachana Shetty <rr27shetth@gmail.com>
2023-04-03 01:26:06 +0530

    First push of public info

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,6 +8,7 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
+    pathPrefix:'/Portfolio',
     siteTitle: `Cara`,
     siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
     siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
